ffd499d9e69689979698a4c73472fc1278492682
Okayolha <okay.olha@gmail.com>
2023-06-05 23:19:29 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,20 +1,23 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { filterContacts } from 'redux/store';
 
 export const Filter = () => {
   const [filter, setFilter] = useState('');
   const dispatch = useDispatch();
+  const contactsFilter = useSelector(state => state.myContacts.filter);
 
   const filterChange = e => {
-    setFilter(e.currentTarget.value);
-    dispatch(filterContacts(e.currentTarget.value));
+    const newFilter = e.currentTarget.value;
+    setFilter(newFilter);
+    dispatch(filterContacts(newFilter));
   };
 
   return (
     <label>
       find contacts by name{' '}
       <input type="text" value={filter} onChange={filterChange} />
+      <div>Current filter: {contactsFilter}</div>
     </label>
   );
 };
